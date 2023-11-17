41c13f3eeeb8f3afdf3aafb10e8dcc131eb6c87d
AlexandreD <alexandre.dosbaa@gmail.com>
2023-11-18 00:12:44 +0100

    mise au format 'fonctionnel' de 'lineage_determination.py'

--- Script/lineage_determination.py
+++ Script/lineage_determination.py
@@ -2,19 +2,15 @@ import re
 import sys 
 import os 
 
-
-ext_variable = sys.argv[1]
-
-distance_A  = "distance_A_result.txt"
-distance_B = "distance_B_result.txt"
-sous_lignee_A = "sub_lineage_A.txt"
-sous_lignee_B = "sub_lineage_B.txt"
-ref_fasta_A = "ref_A.fasta"
-ref_fasta_B = "ref_B.fasta"
-dossier = f"./../9-genetic_distances/attribution_lineage"
-
-
-def import_variable(ext_variable:str)->tuple[str, str, str]:
+def import_variable(
+    ext_variable:str,
+    distance_A:str,
+    sous_lignee_A:str,
+    ref_fasta_A:str,
+    distance_B:str,
+    sous_lignee_B:str,
+    ref_fasta_B:str
+    )->tuple[str, str, str]:
 
     file_name=""
     sous_lignee = ""
@@ -32,10 +28,20 @@ def import_variable(ext_variable:str)->tuple[str, str, str]:
     return file_name, sous_lignee, ref_fasta
 
 
+def lineage_determination_main():
 
-def main():
+    #Global var 
+    ext_variable = sys.argv[1]
 
-    file_name,sous_lignee, ref_fasta = import_variable(ext_variable)
+    distance_A  = "distance_A_result.txt"
+    distance_B = "distance_B_result.txt"
+    sous_lignee_A = "sub_lineage_A.txt"
+    sous_lignee_B = "sub_lineage_B.txt"
+    ref_fasta_A = "ref_A.fasta"
+    ref_fasta_B = "ref_B.fasta"
+    dossier = f"./../9-genetic_distances/lineage_assignment"
+
+    file_name, sous_lignee, ref_fasta = import_variable(ext_variable, distance_A, sous_lignee_A, ref_fasta_A, distance_B, sous_lignee_B, ref_fasta_B)
 
     if os.path.exists(dossier):
         print(f"The '{dossier}' folder exists.")
@@ -46,7 +52,7 @@ def main():
 
     with open(f"./../9-genetic_distances/result_txt/{file_name}","r") as open_file:
     
-        with open (f"./../9-genetic_distances/attribution_lineage/{sous_lignee}","w") as write_file:
+        with open (f"./../9-genetic_distances/lineage_assignment/{sous_lignee}","w") as write_file:
             write_file.write(f"Num ID sub_lineage sub_related_lineage\n")
             
             
@@ -62,7 +68,6 @@ def main():
                 # si les id sont ceux des sequences étudiées, on prend leurs
                 if id_distance.endswith("_A") or id_distance.endswith("_B"):
                     
-                    
 
                     id_distance = ligne[5]
                     id_distance = id_distance[:-1] # pour enlever les ":" à la fin de l'id
@@ -73,25 +78,25 @@ def main():
                 # Distance genetique entre 14_A et KY654518.1: 0.0081626275
                 
                 with open(f"./../references_phylogenie/{ref_fasta}","r") as open_file_ref:
-                    
-                        for nombre_ligne in open_file_ref:
-                            
-                            
-                            
-                            match = re.search(f"{id_distance[:-2]}", f"{nombre_ligne}")
-                            if match :
-                                if id_name.endswith("_A") or id_name.endswith("_B"):
-                                    write_file.write(f"{i} {id_name} {nombre_ligne[1:].split('_')[0]} {id_distance[:-2]}\n")
-                                    print(f"{i} {id_distance[:-2]} {nombre_ligne[1:].split('_')[0]} {id_name}")   
+
+                    for nombre_ligne in open_file_ref:
+                        
+                        
+                        
+                        match = re.search(f"{id_distance[:-2]}", f"{nombre_ligne}")
+                        if match :
+                            if id_name.endswith("_A") or id_name.endswith("_B"):
+                                write_file.write(f"{i} {id_name} {nombre_ligne[1:].split('_')[0]} {id_distance[:-2]}\n")
+                                print(f"{i} {id_distance[:-2]} {nombre_ligne[1:].split('_')[0]} {id_name}")   
 
 
-                                else:
-                                    write_file.write(f"{i} {id_distance[:-2]} {nombre_ligne[1:].split('_')[0]} {'/'}\n")
-                                    print(f"{i} {id_distance[:-2]} {nombre_ligne[1:].split('_')[0]} {'/'}")
+                            else:
+                                write_file.write(f"{i} {id_distance[:-2]} {nombre_ligne[1:].split('_')[0]} {'/'}\n")
+                                print(f"{i} {id_distance[:-2]} {nombre_ligne[1:].split('_')[0]} {'/'}")
 
                                     
                         
-main()
+lineage_determination_main()
 
 
 
